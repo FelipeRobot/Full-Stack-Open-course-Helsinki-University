5be13fd78244adcec2487eb7f3abd96205bdc840
Felipe <felipeuseche22@gmail.com>
2024-12-20 14:14:03 -0500

    Exercise 1.3

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -1,43 +1,56 @@
 const App = () => {
-   
+  
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
 
     /*----------------------------- */
     <div>
-    <Header />
-     <Content />
+    <Header 
+      course={course}
+    />
+     <Content 
+      part1={part1.name}
+      part2={part2.name}
+      part3={part3.name}
+      
+      exercises1={part1.exercises}
+      exercises2={part2.exercises}
+      exercises3={part3.exercises}
+     />
     <Total  />
     </div>
 
 
-
   )
 }
 
-const Header = () => {
+const Header = ({course}) => {
   // const-definitions
-  const course = 'Half Stack application development'
+
 
   return (
     <div>
-      <hi>{course}</hi>
+      <h1>{course}</h1>
     </div>
   )
 }
 
-const Content  = () => {
+const Content  = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
   // const-definitions
   
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
   return (
     <div>
       <p>
